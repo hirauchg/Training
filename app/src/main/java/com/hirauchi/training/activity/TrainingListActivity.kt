bb34533b4bb570cf2554a08a7baad8fc651848a1
hirauchg <gentahirauchi@gmail.com>
2019-05-15 22:11:36 +0900

    トレーニングリスト画面にメニューの実装

--- app/src/main/java/com/hirauchi/training/activity/TrainingListActivity.kt
+++ app/src/main/java/com/hirauchi/training/activity/TrainingListActivity.kt
@@ -1,8 +1,11 @@
 package com.hirauchi.training.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.hirauchi.training.R
 import com.hirauchi.training.fragment.TrainingListFragment
+import org.jetbrains.anko.toast
 
 class TrainingListActivity : BaseActivity() {
 
@@ -10,6 +13,20 @@ class TrainingListActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         supportFragmentManager.beginTransaction().replace(R.id.Container, TrainingListFragment()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_training_list, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_add_training -> toast(R.string.training_list_menu_add_training)
+            R.id.menu_privacy -> toast(R.string.training_list_menu_privacy)
+            R.id.menu_oss -> toast(R.string.training_list_menu_oss)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
